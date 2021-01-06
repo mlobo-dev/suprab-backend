385a0f02dc0dfad84d40e573cafad361c14991d2
Alberto Lopes <alberto_llopes@hotmail.com>
2021-01-06 07:55:26 -0300

    Alterações salvar endereço do membro

---
+++ src/main/java/com/suprab/modules/endereco/mapper/EnderecoCadastroMapper.java
@@ -0,0 +1,16 @@
+package com.suprab.modules.endereco.mapper;
+
+
+import com.suprab.config.mapstruct.BaseMapper;
+import com.suprab.modules.endereco.dto.EnderecoDTO;
+import com.suprab.modules.endereco.entity.Endereco;
+import com.suprab.modules.membro.dto.MembroCadastroDTO;
+import com.suprab.modules.membro.dto.MembroDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface EnderecoCadastroMapper extends BaseMapper<Endereco, MembroCadastroDTO> {
+
+
+}
