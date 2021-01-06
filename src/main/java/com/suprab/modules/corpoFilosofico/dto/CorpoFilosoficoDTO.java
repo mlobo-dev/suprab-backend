3a66a3af7a0ab86d76339d63569beca609466ed4
Alberto Lopes <alberto_llopes@hotmail.com>
2021-01-06 13:42:17 -0300

    Validated membro

--- src/main/java/com/suprab/modules/corpoFilosofico/dto/CorpoFilosoficoDTO.java
+++ src/main/java/com/suprab/modules/corpoFilosofico/dto/CorpoFilosoficoDTO.java
@@ -3,12 +3,23 @@ package com.suprab.modules.corpoFilosofico.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 public class CorpoFilosoficoDTO {
 
     private Long id;
+
+    @NotEmpty(message = "Grau não pode ser vazio")
+    @Size(max=13)
     private String grau;
+
+    @NotEmpty(message = "Corpo não pode ser vazio")
     private String corpo;
+
+    @NotEmpty(message = "Data grau não pode ser vazio")
     private String dataGrau;
 }
--- src/main/java/com/suprab/modules/membro/dto/MembroCadastroDTO.java
+++ src/main/java/com/suprab/modules/membro/dto/MembroCadastroDTO.java
@@ -4,7 +4,11 @@ import com.suprab.modules.corpoFilosofico.dto.CorpoFilosoficoDTO;
 import com.suprab.modules.membro.StatusEnum;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,17 +18,41 @@ public class MembroCadastroDTO {
 
     private Long id;
 
+    @NotNull(message = "Nome não pode ser nulo")
     private StatusEnum status;
+
+    @NotEmpty(message = "Nome não pode ser vazio")
     private String cgp;
+
+    @CPF(message = "Cpf inválido")
     private String cpf;
+
+    @NotEmpty(message = "Nome não pode ser vazio")
     private String nome;
+
+    @NotEmpty(message = "Nome não pode ser vazio")
+    @Size(min=2, max=2)
     private String tipoSanguineo;
+
+    @NotEmpty(message = "Cargo não pode ser vazio")
     private String cargo;
+
+    @NotEmpty(message = "Título honorífico não pode ser vazio")
     private String tituloHonorifico;
+
+    @NotEmpty(message = "Data de nascimento não pode ser vazio")
     private String dataNascimento;
+
+    @NotEmpty(message = "Cep não pode ser vazio")
     private String cep;
+
+    @NotEmpty(message = "Cidade não pode ser vazio")
     private String cidade;
+
+    @NotEmpty(message = "UF não pode ser vazio")
+    @Size(max=2)
     private String uf;
+
     private List<CorpoFilosoficoDTO> corposFilosoficos = new ArrayList<>();
 
 }
